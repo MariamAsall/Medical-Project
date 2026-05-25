0057b0ac16844cd994f8fbce39ec08afadde836f
MariamAsall <mariamasal075@gmail.com>
2026-05-25 23:21:35 +0300

    Updated imports in  medical-core/settings.py

--- medical_core/settings.py
+++ medical_core/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 from pathlib import Path
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
