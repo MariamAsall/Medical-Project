0616491303aa9e7b5db612359dfde4c7d070d132
sami <ahmedsamy121277@gmai.com>
2026-05-26 23:35:21 +0300

    create app appointments and availability aand create models and srealize model and add view and permissions for this views

--- medical_core/settings.py
+++ medical_core/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 import os
 from pathlib import Path
 
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -50,6 +53,8 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'accounts',
+    'appointments',
+    'availability',
 ]
 
 MIDDLEWARE = [
